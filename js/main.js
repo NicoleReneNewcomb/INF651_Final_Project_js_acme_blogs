184b5d594eeb84be83c90a7b88d20bbd1fb85014
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2023-11-23 02:18:05 -0800

    Remove Functions for Testing
    Removed functions used to test project functions.

--- js/main.js
+++ js/main.js
@@ -350,16 +350,3 @@ async function initApp() {
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
-
-// async function jsonData() {
-//     const data = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const json = await data.json();
-//     return json;
-// };
-
-// async function order() {
-//     // const samplePostJSON = await getUserPosts(2);
-//     initApp();
-// };
-
-// order();
